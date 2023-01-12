7068943d48044c2221db701eb8e6a106da89443a
Jan Rosa <102300602+jrosa02@users.noreply.github.com>
2023-01-12 14:33:28 +0100

    Nodes.hpp setup done

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -7,6 +7,7 @@
 
 #include <memory>
 #include <map>
+#include <optional>
 #include "types.hpp"
 #include "storage_types.hpp"
 
@@ -16,7 +17,7 @@ enum RecieverType{
 
 class IPackageReceiver{
 public:
-    vitrual void receive_package(Package&& p) = 0;
+    virtual void receive_package(Package&& p) = 0;
     virtual ElementID get_id() const = 0;
 
 };
@@ -26,28 +27,43 @@ public:
     Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d);
 };
 
-using preferences_t = std::map<IPackageReceiver*, ProbabilityGenerator>
+//FIXME nie wiem co za typ te preferencje
+using preferences_t = std::map<IPackageReceiver*, ProbabilityGenerator>;
+
 class ReceiverPreferences{
 public:
     ReceiverPreferences(ProbabilityGenerator pg);
     void add_receiver(IPackageReceiver* r);
     void remove_receiver(IPackageReceiver* r);
     IPackageReceiver* choose_receiver();
-    preferences
+    preferences_t& get_preferences() ;
 
 private:
-    preferences_t;
+    preferences_t preferences;
 };
 
 class PackageSender{
-
+public:
+    PackageSender(PackageSender&& ps);
+    void send_package();
+    std::optional<Package>& get_sending_buffer() const;
+protected:
+    void push_package(Package&& aPackage);
 };
 
 class Ramp{
-
+public:
+    Ramp(ElementID id, TimeOffset di);
+    void deliver_goods(Time t);
+    TimeOffset get_delivery_interval() const;
+    ElementID get_id() const;
 };
 
 class Worker{
-
+public:
+    Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q);
+    void do_work(Time t);
+    TimeOffset get_processing_duration() const;
+    Time get_package_processing_start_time() const;
 };
 #endif //ZPO_SERWERY_NODES_HPP
